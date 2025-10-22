01e5a00104f67313d6a3607b02e28a20a034e888
JuanGzlz <gonzalez-juan@correo.unimet.edu.ve>
2025-10-21 21:36:51 -0400

    Primitivas

---
+++ src/main/java/main/ProyectoSO_2526.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author Juan I Gonzalez
+ */
+public class ProyectoSO_2526 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
